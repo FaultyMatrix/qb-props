a1037b9bb6e4d448f565dea6955ab04d790f1eb5
Spinks <95166538+FaultyMatrix@users.noreply.github.com>
2022-01-03 10:22:25 +1100

    Updated main.lua

--- client/main.lua
+++ client/main.lua
@@ -1,22 +1,23 @@
-function PropSpawn()
-    for k,v in pairs (Config.Props) do
-        RequestModel(v.model)
-        local iter_for_request = 1
-        while not HasModelLoaded(v.model) and iter_for_request < 5 do
-            Wait(500)                
-            iter_for_request = iter_for_request + 1
-        end
-        if not HasModelLoaded(v.model) then
-            SetModelAsNoLongerNeeded(v.model)
-        else
-            local ped = PlayerPedId()
-            local created_object = CreateObjectNoOffset(v.model, v.coords.x, v.coords.y, v.coords.z - 1, 1, 0, 1)
-            PlaceObjectOnGroundProperly(created_object)
-            SetEntityHeading(created_object, v.coords.w)
-            FreezeEntityPosition(created_object, true)
-            SetModelAsNoLongerNeeded(v.model)
+CreateThread(function()
+    function PropSpawn()
+        for k,v in pairs (Config.Props) do
+            RequestModel(v.model)
+            local iter_for_request = 1
+            while not HasModelLoaded(v.model) and iter_for_request < 5 do
+                Wait(500)                
+                iter_for_request = iter_for_request + 1
+            end
+            if not HasModelLoaded(v.model) then
+                SetModelAsNoLongerNeeded(v.model)
+            else
+                local ped = PlayerPedId()
+                local created_object = CreateObjectNoOffset(v.model, v.coords.x, v.coords.y, v.coords.z - 1, 1, 0, 1)
+                PlaceObjectOnGroundProperly(created_object)
+                SetEntityHeading(created_object, v.coords.w)
+                FreezeEntityPosition(created_object, true)
+                SetModelAsNoLongerNeeded(v.model)
+            end
         end
     end
-end
-
-PropSpawn()
+    PropSpawn()
+end)
