ce65bfa7e4bab8d1ad28ff8d537ed796b169a943
T0stbrot <51487454+T0stbrot@users.noreply.github.com>
2023-11-27 17:57:43 +0100

    delete objects at resource stop
    please test

--- client/main.lua
+++ client/main.lua
@@ -1,3 +1,5 @@
+local list = {}
+
 CreateThread(function()
     for k,v in pairs (Config.Props) do
         RequestModel(v.model)
@@ -11,10 +13,18 @@ CreateThread(function()
         else
             local ped = PlayerPedId()
             local created_object = CreateObjectNoOffset(v.model, v.coords.x, v.coords.y, v.coords.z - 1, 1, 0, 1)
+            table.insert(list, created_object)
             PlaceObjectOnGroundProperly(created_object)
             SetEntityHeading(created_object, v.coords.w)
             FreezeEntityPosition(created_object, true)
             SetModelAsNoLongerNeeded(v.model)
         end
     end
+end)
+
+AddEventHandler('onResourceStop', function(resourceName)
+    if (GetCurrentResourceName() ~= resourceName) then
+      return
+    end
+    for i=1, #list do DeleteObject(list[i]) end
 end)
